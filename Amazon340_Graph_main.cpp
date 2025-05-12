bf42672111451f071a95f4eb0456648e9f1358bc
John-Tsiglieris <121003125+John-Tsiglieris@users.noreply.github.com>
2025-05-11 20:44:26 -0700

    Update

--- Amazon340_Graph_main.cpp
+++ Amazon340_Graph_main.cpp
@@ -9,8 +9,24 @@
 using namespace std;
 int main() {
 	// Create a list of Users
+	
 	// Add 10 distinct products to the vector
-	// TO DO 
+	// Create an adjacency list?
+	LinkedBag<Product> distinctProducts;
+	distinctProducts.add(Product("Product1", "Description1", 3, 14));
+	distinctProducts.add(Product("Product2", "Description2", 4.5, 11));
+	distinctProducts.add(Product("Product3", "Description3", 5.5, 38));
+	distinctProducts.add(Product("Product4", "Description4", 4.5, 22));
+	distinctProducts.add(Product("Product5", "Description5", 7.7, 31));
+	distinctProducts.add(Product("Product6", "Description6", 1.3, 75));
+	distinctProducts.add(Product("Product7", "Description7", 0.4, 45));
+	distinctProducts.add(Product("Product8", "Description8", 5.5, 27));
+	distinctProducts.add(Product("Product9", "Description9", 9, 101));
+	distinctProducts.add(Product("Product10", "Description10", 7, 100));
+	
+
+	
+	
 
 	// Initialize a graph with n vertices (where n is the number of Products) 
 	//	 Indicate whether it is directed or undirected 
--- TODO.md
+++ TODO.md
@@ -1,1 +1,2 @@
-1. Change addEdge() to use append instead of push_back
+~~1. Change addEdge() to use append instead of push_back~~
+2. Wtf does line 11 and 12 mean in the main file? What is a list of users
